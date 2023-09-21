4420818b460096ea1c5b5b31df02eb9c37a7b4c9
sofips <sofiperon98@gmail.com>
2023-09-21 19:30:15 -0300

    fix norm check

--- rl_torch.py
+++ rl_torch.py
@@ -112,7 +112,7 @@ class MyEnv(Env):
         else:
             done = False
 
-        if abs(la.norm(self.cstate) - 1.)>1E8:
+        if abs(la.norm(self.cstate) - 1.)>1E-8:
             print('FALLO EN LA NORMALIZACION',la.norm(self.cstate))
 
         info = {}
