07fd62141e8ec16575548b6b79408e4834a37a7b
sofi <sofiperon98@gmail.com>
2025-02-08 15:30:57 -0300

    implement variable rewards and actions set

--- config_experiment.py
+++ config_experiment.py
@@ -3,29 +3,28 @@
 #####################################################################
 
 import configparser
-import os
 import sys
 
 cname = sys.argv[1]
 config = configparser.ConfigParser()
 
-#-----------------------------------------------------------------#
+# -----------------------------------------------------------------#
 #                         SYSTEM PARAMETERS                       #
-#-----------------------------------------------------------------#
+# -----------------------------------------------------------------#
 
 chain_length = 10
-tstep_length = 0.15                       
-tolerance = 0.05                     
-max_t_steps = 50                    
+tstep_length = 0.15
+tolerance = 0.05
+max_t_steps = 50
 field_strength = 100
 coupling = 1
 n_actions = 16
 
-#-----------------------------------------------------------------#
+# -----------------------------------------------------------------#
 #                    LEARNING HYPERPARAMETERS                     #
-#-----------------------------------------------------------------#
+# -----------------------------------------------------------------#
 
-number_of_features = 2*chain_length
+number_of_features = 2 * chain_length
 number_of_episodes = 50000
 step_learning_interval = 5
 
@@ -33,9 +32,9 @@ learning_rate = 0.001
 gamma = 0.95
 
 # memory
-replace_target_iter=200
-memory_size=40000
-batch_size=32
+replace_target_iter = 200
+memory_size = 40000
+batch_size = 32
 
 # epsilon
 epsilon = 0.99
@@ -44,20 +43,29 @@ epsilon_minimum = 0.01
 
 # dqn
 fc1_dims = 50
-fc2_dims = fc1_dims//3
-dropout = 0.05
+fc2_dims = 50
+dropout = 0.0
 
-#---------------------------------------------------
+
+reward_function = "original"  # "original" , "full reward", "ipr"
+action_set = "zhang"  # "zhang", "oaps" (action per site)
+n_actions = (
+    16 if action_set == "zhang" else chain_length + 1
+    if action_set == "oaps" 
+    else 0
+)
+
+# ---------------------------------------------------
 
 config["system_parameters"] = {
     "chain_length": str(chain_length),
     "tstep_length": str(tstep_length),
     "tolerance": str(tolerance),
     "max_t_steps": str(max_t_steps),
-    "field_strength" : str(field_strength),
+    "field_strength": str(field_strength),
     "coupling": str(coupling),
-    "n_actions": str(n_actions)
-
+    "n_actions": str(n_actions),
+    "action_set": action_set,
 }
 
 config["learning_parameters"] = {
@@ -66,18 +74,18 @@ config["learning_parameters"] = {
     "learning_rate": str(learning_rate),
     "gamma": str(gamma),
     "replace_target_iter": str(replace_target_iter),
-    "memory_size" : str(memory_size),
+    "memory_size": str(memory_size),
     "batch_size": str(batch_size),
     "epsilon": str(epsilon),
     "epsilon_decay": str(epsilon_decay),
     "epsilon_minimum": str(epsilon_minimum),
     "fc1_dims": str(fc1_dims),
     "fc2_dims": str(fc2_dims),
-    "dropout": str(dropout)
-
+    "dropout": str(dropout),
+    "reward_function": reward_function,
 }
 
-#---------------------------------------------------
+# ---------------------------------------------------
 
 config_name = cname + ".ini"
 
--- environment.py
+++ environment.py
@@ -45,7 +45,9 @@ class MyEnv(Env):
     def __init__(self, config_file):
 
         # read system parameters from file
-        config = configparser.ConfigParser()
+        self.config = configparser.ConfigParser()
+        config = self.config
+
         config.read(config_file)
 
         self.n = config.getint("system_parameters", "chain_length")
@@ -53,50 +55,63 @@ class MyEnv(Env):
         self.tolerance = config.getfloat("system_parameters", "tolerance")
         self.max_t_steps = config.getint("system_parameters", "max_t_steps")
         self.coupling = config.getfloat("system_parameters", "coupling")
-        self.field_strength = config.getfloat("system_parameters",
-                                              "field_strength")
+        self.field_strength = config.getfloat("system_parameters", "field_strength")
         self.n_actions = config.getint("system_parameters", "n_actions")
+        self.action_set = config.get("system_parameters", "action_set")
+        reward_function = config.get("learning_parameters", "reward_function")
 
         # --------------------------------------------------------------------------
         # Define action and observation space (complex observations)
         # --------------------------------------------------------------------------
         self.action_space = Discrete(self.n_actions)  # 16 acciones posibles
-        self.observation_space = Box(low=np.zeros(2 * self.n),
-                                     high=np.ones(2 * self.n))
+        self.observation_space = Box(low=np.zeros(2 * self.n), high=np.ones(2 * self.n))
         # --------------------------------------------------------------------------
         # Define matrices to store actions, and propagations
         # (also check spect. decomp.)
         # --------------------------------------------------------------------------
-        self.action_mat = actions_zhang(self.field_strength, self.coupling,
-                                        self.n)
-        self.energies = np.zeros((16, self.n), dtype=np.complex_)
-        self.bases = np.zeros((16, self.n, self.n), dtype=np.complex_)
-        self.propagators = np.zeros((16, self.n, self.n), dtype=np.complex_)
-        self.sp_desc = np.zeros((16, self.n, self.n), dtype=np.complex_)
+        if self.action_set == "zhang":
+            self.action_mat = actions_zhang(self.field_strength, self.coupling, self.n)
+        elif self.action_set == "oaps":
+            self.action_mat = one_field_actions(self.field_strength, self.coupling, self.n)
+        else:
+            raise ValueError("Invalid action set. Choose 'zhang' or 'oaps'.")
+
+        self.energies = np.zeros((self.n_actions, self.n), dtype=np.complex_)
+        self.bases = np.zeros((self.n_actions, self.n, self.n), dtype=np.complex_)
+        self.propagators = np.zeros((self.n_actions, self.n, self.n), dtype=np.complex_)
+        self.sp_desc = np.zeros((self.n_actions, self.n, self.n), dtype=np.complex_)
 
         # ---------------------------------------------------------------------------
         # Calculate propagators using exponentials
         # ---------------------------------------------------------------------------
 
         for i in range(0, self.n_actions):
-
-            self.energies[i, :], self.bases[i, :, :] = la.eig(
-                self.action_mat[i, :, :])
-            self.propagators[i, :, :] = expm(-1j * self.action_mat[i] *
-                                             self.dt)
+            self.energies[i, :], self.bases[i, :, :] = la.eig(self.action_mat[i, :, :])
+            self.propagators[i, :, :] = expm(-1j * self.action_mat[i] * self.dt)
 
             for k in range(0, self.n):
                 p = np.outer(self.bases[i, :, k], self.bases[i, :, k])
-                self.sp_desc[i, :, :] = self.sp_desc[i, :, :] + p * \
-                    self.energies[i, k]
+                self.sp_desc[i, :, :] = self.sp_desc[i, :, :] + p * self.energies[i, k]
 
         # Spectral Decomposition Check
         check_sd(self.n, self.n_actions, self.action_mat, self.sp_desc)
         # Correct Propagation Check
         propagators_check(
-            self.n, self.n_actions, self.dt, self.propagators, self.bases,
-            self.energies
+            self.n, self.n_actions, self.dt, self.propagators, self.bases, self.energies
         )
+
+        # ---------------------------------------------------------------------------
+        # Define reward function
+        # ---------------------------------------------------------------------------
+
+        if reward_function == "full reward":
+            self.reward = full_reward
+        elif reward_function == "original":
+            self.reward = original_reward
+        elif reward_function == "ipr":
+            self.reward = ipr_reward
+        else:
+            raise ValueError("Invalid reward function. Choose 'full reward', 'original' or 'ipr'.")
 
         # Set time to 0 and states to one excitation
 
@@ -122,7 +137,7 @@ class MyEnv(Env):
             done (bool): Whether the episode is done or not.
 
         """
-
+        config = self.config
         self.t_step = self.t_step + 1
 
         j = 0
@@ -136,27 +151,22 @@ class MyEnv(Env):
             self.state[i] = np.real(self.cstate[i // 2])
             self.state[i + 1] = np.imag(self.cstate[i // 2])
 
-        fid = np.real(self.cstate[self.n - 1] *
-                      np.conjugate(self.cstate[self.n - 1]))
-
-        if fid <= 0.8:
-            reward = 10 * fid
-        elif 0.8 <= fid <= 1 - self.tolerance:
-            reward = 100 / (1 + np.exp(10 * (1 - self.tolerance - fid)))
-        else:
-            reward = 2500
+        fid = np.real(self.cstate[self.n - 1] * np.conjugate(self.cstate[self.n - 1]))
+        
+        reward = self.reward(self.state,
+                             self.propagators[action, :, :],
+                             self.t_step,
+                             config)
 
         if (fid >= 1.0 - self.tolerance) or (self.t_step >= self.max_t_steps):
             done = True
         else:
             done = False
-
+        
         # check state normalization
         if abs(la.norm(self.cstate) - 1.0) > 1e-8:
-            print("Normalization failed!!!!", la.norm(self.cstate))
-            quit()
-
-        reward = reward * (0.95**self.t_step)
+            raise ValueError(f"""State not normalized. 
+                             Norm: {la.norm(self.cstate)}""")
 
         return self.state, self.cstate, self.t_step, fid, reward, done
 
@@ -205,7 +215,6 @@ def check_sd(n, n_actions, actions, sd):
     for k in np.arange(0, n_actions):
         for i in np.arange(0, n):
             for j in np.arange(0, n):
-
                 if actions[k, i, j] - sd[k, i, j] > 1e-8:
                     print("error in spectral decomposition")
                     check_sd = False
@@ -274,6 +283,7 @@ def actions_zhang(field_strength, coupling, chain_length):
     Returns:
         numpy.ndarray: A matrix of actions with shape (16, chain_length,
         chain_length).
+
     """
     nc = 3  # number of control sites, nc=3,there are totally 16 actions.
 
@@ -314,10 +324,121 @@ def actions_zhang(field_strength, coupling, chain_length):
 
     for actions in mag:
         ham = (
-            np.diag([coupling for i in range(chain_length - 1)], 1) * (1-0j)
-            + np.diag([coupling for i in range(chain_length - 1)], -1) * (1+0j)
+            np.diag([coupling for i in range(chain_length - 1)], 1)*(1 - 0j)
+            + np.diag([coupling for i in range(chain_length - 1)], -1)*(1 + 0j)
             + np.diag(actions)
         )
         action_hamiltonians[mag.index(actions)] = ham
 
     return action_hamiltonians
+
+def one_field_actions(bmax, coupling, chain_length):
+    """
+    Generates a set of action matrices corresponding to fields acting on every individual
+    site.
+
+    Parameters:
+    bmax (float): The maximum value of the field, used in diagonal elements.
+    chain_length (int): Chain length, which defines the size of action matrices.
+
+    Returns:
+    numpy.ndarray: A 3D numpy array of shape (chain_length + 1, chain_length, chain_length) representing the action matrices.
+    """
+
+    action_matrices = np.zeros((chain_length + 1, chain_length, chain_length))
+    J = coupling
+
+    for i in range(0, chain_length):
+
+        for k in range(0, chain_length - 1):
+            action_matrices[i + 1, k, k + 1] = J
+            action_matrices[i + 1, k + 1, k] = action_matrices[i + 1, k, k + 1]
+
+        action_matrices[i + 1, i, i] = bmax
+
+    for k in range(0, chain_length - 1):
+        action_matrices[0, k, k + 1] = J
+        action_matrices[0, k + 1, k] = action_matrices[0, k, k + 1]
+
+    return action_matrices
+
+
+# # ---------------------------------------------------------
+#                       Reward functions
+# # ---------------------------------------------------------
+
+
+def full_reward(next_state, prop, time_step, config):
+
+    max_time = config.getint("system_parameters", "max_t_steps")
+    gamma = config.getfloat("learning_parameters", "gamma")
+    tolerance = config.getfloat("system_parameters", "tolerance")
+
+    fidelity_evolution = state_fidelity(next_state)
+
+    for step in range(time_step, max_time):
+        next_state = prop * next_state  # do operation
+        fidelity_evolution = np.append(fidelity_evolution,
+                                       state_fidelity(next_state))
+
+    max_fid = np.max(fidelity_evolution)
+
+    # reward function
+    if max_fid < 0.8:
+        reward = max_fid * 10
+
+    if max_fid >= 0.8 and max_fid <= tolerance:
+        reward = 100 / (1 + np.exp(10 * (tolerance - max_fid)))
+
+    if max_fid > tolerance:
+        reward = 2500 * max_fid
+
+    reward = reward * (gamma**time_step)
+    # a discount is given with respected to step
+
+    return reward
+
+
+def original_reward(next_state, prop, time_step, config):
+
+    gamma = config.getfloat("learning_parameters", "gamma")
+    tolerance = config.getfloat("system_parameters", "tolerance")
+
+    fidelity = state_fidelity(next_state)
+
+    if fidelity < 0.8:
+        reward = fidelity * 10
+    if fidelity >= 0.8 and fidelity <= tolerance:
+        reward = 100 / (1 + np.exp(10 * (tolerance - fidelity)))
+    if fidelity > tolerance:
+        reward = 2500
+
+    reward = reward * (gamma**time_step)
+
+    return reward
+
+
+def ipr_reward(next_state, prop, time_step, config):
+
+    gamma = config.getfloat("learning_parameters", "gamma")
+
+    ipr = calc_ipr(next_state)
+    fidelity = state_fidelity(next_state)
+    reward = fidelity / ipr
+
+    reward = reward * (gamma**time_step)
+
+    return reward
+
+
+def state_fidelity(state):
+    return np.asarray((abs(state[-1]) ** 2))  # calculate fidelity
+
+
+def calc_ipr(state):
+    nh = np.shape(state)[0]
+    ipr = 0
+    for i in range(nh):
+        ipr += np.real(state[i] * np.conjugate(state[i])) ** 2
+    return 1 / ipr
+
